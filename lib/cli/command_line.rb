f2c88abaace8ed55f52e9f32a90381e4589d7fcc
lieby weintraub <liebahinda@gmail.com>
2019-04-14 17:12:17 +0000

    add cli

--- lib/cli/command_line.rb
+++ lib/cli/command_line.rb
@@ -4,7 +4,11 @@ class CommandLineInterface
     input=" "
     puts "Welcome to the library finder!"
     puts "I would love to help you find the library you are looking for"
-    puts "To list all the libraries by name, enter 'list libraries'"
+    puts "To list all the libraries, enter 'list libraries'"
+    puts "To list all the Libraries by city, enter 'list by city'"
+    puts "To get the location of each library, enter 'get location' "
+    puts "To get the zip code of each library, enter 'get zip code'"
+    puts "To get the phone number of each library, enter 'get phone number'"
     while input != "exit"
       input=gets.strip
       case input 
@@ -17,7 +21,7 @@ end
   def list_libraries
  libraries = Library.all.sort_by {|library| library.name}
     libraries.each_with_index do |library, index| 
-    puts "#{index + 1 }. #{library.name}"
+    puts "#{index + 1 }. #{library.city}, #{library.name}, #{library.location}, #{library.zip}, #{library.phone}"
   end 
 end 
 end 
