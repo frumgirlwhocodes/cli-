9a7569abbfaf4c54a780251417f0964ba4d625fb
lieby weintraub <liebahinda@gmail.com>
2019-04-15 02:51:08 +0000

    add data

--- lib/cli/command_line.rb
+++ lib/cli/command_line.rb
@@ -5,7 +5,7 @@ class CommandLineInterface
     puts "Welcome to the library finder!"
     puts "I would love to help you find the library you are looking for and any other information you may need"
     puts "To list all the libraries, enter 'list libraries'"
-    puts "To list all the libraries by city, enter 'list by city'"
+    puts "To list all the libraries by a certian city, enter 'list by city'"
     puts "To get the location of a library, enter 'get location' "
     puts "To get the zip code of a library, enter 'get zip code'"
     puts "To get the phone number of a library, enter 'get phone number'"
@@ -33,7 +33,16 @@ end
   end 
 end 
 def list_by_city 
-  
-  
+  puts "please enter the name of an city:" 
+  input=get.chomp 
+  new_array=[]
+  Library.all do |library| 
+    if library.city == input 
+      new_array << library 
+    end  
+    libraries=new_array.sort_by {|library| library.name} 
+    libraries.each_with_index |library, index|
+    puts "#{index + 1}.#{library.name} - #{library.city}"
+  end 
 end 
 end 
