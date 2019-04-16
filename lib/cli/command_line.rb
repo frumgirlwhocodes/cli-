9bc9e0744d8facf01e808ecbe41e9c80063631fa
lieby weintraub <liebahinda@gmail.com>
2019-04-16 04:03:27 +0000

    add data

--- lib/cli/command_line.rb
+++ lib/cli/command_line.rb
@@ -4,18 +4,18 @@ class CommandLineInterface
    Scraper.new.create_libraries
     puts "Welcome to the library finder!"
     puts "I would love to help you find the library you are looking for and any other information you may need"
-    puts "Here is a list of all the libraries"
+    puts "Here is a list of all the libraries:"
     start 
   end 
    def start 
-     list_libraries
+     list
      puts ""
     puts "What number library would you like to see more information on?"
     
     more_info
     
   puts ""
-    puts "Would you like to see another restaurant? Enter Y or N"
+    puts "Would you like to select another library from the list? Enter Y or N"
 
     input = gets.strip.downcase
     if input == "y"
@@ -30,22 +30,20 @@ class CommandLineInterface
       start
     end
   end
-    
-    
-     def more_info
-     input = gets.strip
-      libraries = Library.all.sort_by {|library| library.name}
-    libraries.each.with_index(1) do |library, index|
-      if index  == input.to_i
-        puts " The city, address, zipcode and telephone number for this library is: #{library.city}, #{library.address}, #{library.zip}, #{library.phone} "
-     end    
-    end 
-   
-    def list_libraries
+   def list
     #libraries= Library.all.sort_by { |library| library.name}
-    Libary.all.each.with_index(1) do |library, index|
+    Library.all.each.with_index(1) do |library, index|
     puts "#{index}. #{library.name} "
   end
 end 
 end 
+    
+    
+     def more_info
+     input = gets.strip.to_i
+    Library.all.each.with_index(1) do |library, index|
+      if index  == input
+        puts " The city, address, zipcode and telephone number for this library is: #{library.city}, #{library.address}, #{library.zip}, #{library.phone} "
+     end    
+    end 
 end 
