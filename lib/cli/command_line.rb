c14107debe6d37748b7bad312c5c7187b7c820b9
lieby weintraub <liebahinda@gmail.com>
2019-04-16 05:00:10 +0000

    finished project

--- lib/cli/command_line.rb
+++ lib/cli/command_line.rb
@@ -2,13 +2,15 @@ class CommandLineInterface
 
  def call 
    Scraper.new.create_libraries
+   list
+   puts "Above is a list of all the libraries"
     puts "Welcome to the library finder!"
     puts "I would love to help you find the library you are looking for and any other information you may need"
-    puts "Here is a list of all the libraries:"
+    puts 
     start 
   end 
    def start 
-     list
+     
      puts ""
     puts "What number library would you like to see more information on?"
     
